71e0e065dcc0edc4755d7424c08482b75a2cc570
martin <koo.martin@gmail.com>
2024-12-03 11:19:27 -0500

    env vars

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
-const DB_URL = "http://localhost:9000/exec"
+import { DB_URL } from '$env/static/private';
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = async () => {
     // TODO: group by day and pad up to current day
     // const query = "SELECT * FROM events ORDER BY timestamp DESC"
     const query = "SELECT timestamp, SUM(amount) FROM events SAMPLE BY 1d FILL(0) ALIGN TO CALENDAR TIME ZONE 'EST' ORDER BY timestamp DESC LIMIT 300;"
-    const res = await fetch(`${DB_URL}?query=${encodeURI(query)}`);
+    const res = await fetch(`${DB_URL}/exec?query=${encodeURI(query)}`);
 
     return await res.json();
 };
--- src/routes/events/+server.ts
+++ src/routes/events/+server.ts
@@ -1,13 +1,12 @@
 import { json } from '@sveltejs/kit';
-
-const DB_URL = "http://localhost:9000/exec";
+import { DB_URL } from '$env/static/private';
 
 export const POST = async ({ request }: { request: Request}) => {
     const { topic } = await request.json();
 
     if(!topic) return json({ status: 400 })
     const query = `INSERT INTO events (id, topic, amount, timestamp) VALUES (rnd_uuid4(), '${ topic }', 1, now())`;
-    const res = await fetch(`${DB_URL}?query=${encodeURI(query)}`);
+    const res = await fetch(`${DB_URL}/exec?query=${encodeURI(query)}`);
     console.log(res)
     return await res.json();
 };
