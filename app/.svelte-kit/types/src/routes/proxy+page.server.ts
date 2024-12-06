6e7915d3a9ebb90d4a5edf0bc670cc730ba7eb48
martin <koo.martin@gmail.com>
2024-12-06 15:25:23 -0500

    move app into app folder

---
+++ app/.svelte-kit/types/src/routes/proxy+page.server.ts
@@ -0,0 +1,35 @@
+// @ts-nocheck
+import fs from 'fs';
+
+const CONFIG = JSON.parse(fs.readFileSync('config.json').toString());
+
+/** */
+export const load = async () => {
+    const data: { columns: Array<string>, topics: { [key:string]: any } } = {
+        columns: [],
+        topics: {}
+    };
+
+    for(let topic of CONFIG.topics) {
+        // USE UTC OFFSETS, ZERO REASON TO USE TIMEZONE NAMES UNDER DST
+        let query = `
+        SELECT
+            timestamp, SUM(amount), topic
+        FROM
+            ${CONFIG.tablename}
+        WHERE
+            topic='${topic}'
+        SAMPLE BY
+            1d FILL(0) ALIGN TO CALENDAR TIME ZONE '-05:00'
+        ORDER BY
+            timestamp DESC
+        LIMIT
+            300;`;
+        let res = await fetch(`${CONFIG.dbUrl}/exec?query=${encodeURIComponent(query)}`);
+        let resData = await res.json();
+        (data.topics as { [key: string]: any})[topic as string] = (resData).dataset;
+        data.columns = resData.columns;
+    }
+
+    return data;
+};
